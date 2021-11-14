3ef1dfd0e6dd3b3344e7127d2479f087b65dd0b6
Dom D'Attilio <ddattilio20@gmail.com>
2021-11-14 13:33:57 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -40,7 +40,7 @@ function runTests()
     //Test 4
     console.log("Test 4: Does seperateCases() throw error if removing case not in array: ");
     try{
-        seperateCase(6, testArray);
+        chooseCase(6, testArray);
         console.log("Failed.\n");
     }
     catch {
