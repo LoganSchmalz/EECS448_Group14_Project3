fe720b984a1dbfaa8e94837ef7f6525cc3892b5e
Dom D'Attilio <ddattilio20@gmail.com>
2021-11-15 17:48:07 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -38,7 +38,6 @@ function runTests()
 
 
     //Test 4
-    let testArray = [1,2,3,4,5];
     console.log("Test 4: Does randomizeCases() shuffle array:  ");
     randomizeCases(testArray);
     randomizeCases(testArray);
