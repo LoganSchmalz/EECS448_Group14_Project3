65e6e0fa8f6de7cb6fe2522c10a54472f7789947
Dom D'Attilio <ddattilio20@gmail.com>
2021-11-15 21:46:17 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -44,14 +44,7 @@ function runTests()
     else console.log("Passed.\n");
 
 
-    //Test 5
-    console.log("Test 5: Does resetUS() reset the case array: ");
-    resetUS();
-    if(cases ==  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26])
-    {
-        console.log("Passed.\n");
-    }
-    else console.log('Failed.\n');
+   
 
         
     
