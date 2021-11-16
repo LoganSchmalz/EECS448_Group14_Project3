be92089194d94046f7bb387c218cb7875a946e4a
Dom D'Attilio <ddattilio20@gmail.com>
2021-11-15 18:13:53 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -25,7 +25,7 @@ function runTests()
     }
     else console.log("Failed.\n")
 
-
+/*
     //Test 3
     console.log("Test 3: Does seperateCases() remove a value while preserving order: ");
     let testArray = [1,2,3,4,5];
@@ -35,19 +35,15 @@ function runTests()
         console.log("Passed.\n");
     }
     else console.log("Failed.\n");
-
+*/
 
     //Test 4
-    console.log("Test 4: Does randomizeCases() shuffle array:  ");
-    randomizeCases(testArray);
-    randomizeCases(testArray);
-    randomizeCases(testArray);
-    if(testArray == [1,2,3,4,5])
-    {
-        console.log("Failed.\n");
+    console.log("Test 4: Does formatMoneyUS() return a monetary value, while keeping the number the same: ");
+    let testNum = formatMoneyUS(56430);
+    if(testNum == "$56,430"){
+        console.log("Passed.\n");
     }
-    else console.log("Passed.\n");
-
+    else console.log("Failed.\n");
 
 
 
