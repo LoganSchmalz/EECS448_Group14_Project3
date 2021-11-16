e1a60f48c6d1767fc233fccd0d0b9324bab8bcc1
Dom D'Attilio <ddattilio20@gmail.com>
2021-11-15 21:19:33 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -43,15 +43,17 @@ function runTests()
     }
     else console.log("Passed.\n");
 
+
     //Test 5
     console.log("Test 5: Does randomizeCases throw error on improper value: ");
-    try {
-        randomizeCases(5);
+    randomizeCases(5);
+    /*try {
+
         console.log("Failed.\n");
     } catch {
         console.log("Passed.\n");
     }
-        
+        */
     
 
 }
