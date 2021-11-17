73d936e1bd60b0ee71bed9fc8f0b7d630ae28a1c
Dom D'Attilio <ddattilio20@gmail.com>
2021-11-16 22:07:24 -0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,4 +1,10 @@
-//Test File for running Test Suite
+
+
+
+/**
+ * @description This is the test method. To run tests, call runTests() in console. It will create new games for both US rules and UK rules 
+ * when necessary.
+ */
 
 function runTests()
 {
@@ -7,6 +13,7 @@ function runTests()
 
     //Test 1: Does expectedPayout return a non-zero, positive number
     console.log("Test 1: Does expectedPayout() return a non-zero, positive number for US: ");
+     //Creates new game with US rules
     gameplay(1);
     let expecVal_testUS = expectedPayout(1);
     if(expecVal_testUS > 0 )
@@ -18,6 +25,7 @@ function runTests()
 
     //Test 2: Does expectedPayout return a non-zero, positive number
     console.log("Test 2: Does expectedPayout() return a non-zero, positive number for UK: ");
+    //Creates new game with UK rules
     gameplay(2);
     let expecVal_testUK = expectedPayout(2);
     if(expecVal_testUK > 0 )
@@ -28,6 +36,7 @@ function runTests()
 
     //Test 3: Does bankOffer return a non zero positive number
     console.log("Test 3: Does bankOffer() return a non-zero, positive number for US: ");
+     //Creates new game with US rules
     gameplay(1);
     let bankOffer_testUS = bankOffer(1);
     
@@ -40,6 +49,7 @@ function runTests()
 
       //Test 4: Does bankOffer return a non zero positive number
       console.log("Test 4: Does bankOffer() return a non-zero, positive number for UK: ");
+       //Creates new game with UK rules
       gameplay(2);
       let bankOffer_testUK = bankOffer(2);
       
